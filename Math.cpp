b726e3c30e5cb2be658989ef54eb5fd270b3b159
Mariyam-Suraiya <mariyamsuraiya2@gmail.com>
2023-09-16 16:23:49 +0600

    math

---
+++ Math.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+
+    int x; // Here is an declaration
+
+    x = 5; // give in variable
+
+    std::cout << x << std::endl;
+
+    std::cout << "For more example 👉👇" << std::endl;
+
+    int m = 30;
+    int n = 4;
+    int sum = m * n;
+
+    std::cout << m << '\n';
+    std::cout << n << '\n';
+    std::cout << sum << '\n';
+
+    std::cout << "int don't give any decimal numbers!!" << std::endl;
+
+    // integer(Whole number)
+
+    int Year = 3;
+    int Month = 5.9;
+    int Days = 7;
+
+    std::cout << Year;
+    std::cout << "<";
+    std::cout << Month;
+    std::cout << "<";
+    std::cout << Days;
+
+    return 0;
+}
