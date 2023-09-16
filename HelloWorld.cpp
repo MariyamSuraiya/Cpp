08cb931d55711d9711e56ce029704f16ad3e2703
Mariyam-Suraiya <mariyamsuraiya2@gmail.com>
2023-09-16 15:08:16 +0600

    first repo in C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+
+    // This is my comment...I can also comment with Slash multiflication..Like--
+
+    /*
+    I am new c++ learner..Now I want to try practise with simple c++ language code.
+    */
+    std::cout << "I am learning c++!" << std::endl;
+    std::cout << "For giving one code there is looking problem!" << '\n';
+    std::cout << "So now i am giving two more code for try to solving or remove problem!" << std::endl;
+
+    std::cout << "Now I can run code without any problem!!!" << std::endl;
+
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "Do you??";
+    std::cout << "I love it so much!!" << '\n';
+    std::cout << "Now tell me do you like it??";
+
+    return 0;
+}
